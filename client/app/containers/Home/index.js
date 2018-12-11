e667e7b8a331681940c32472fbff5261d3597483
Yasser Zubair <yasserzubair68@gmail.com>
2018-12-12 02:18:45 +0500

    fixed tiles and background

--- client/app/containers/Home/index.js
+++ client/app/containers/Home/index.js
@@ -13,20 +13,24 @@ class Home extends React.Component {
     return (
 		<div>
 			<div className="home-screen" style={{paddingLeft: '0', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-				<img className="background-image" src={require('../../../site-specs/sliced-images/background-bg.png')} />
+				<div className={style.homeCoverStyle} style={{background: `url(${require('../../../site-specs/sliced-images/background-bg.png')})`}}>
+				</div>
 					<p style={{color: 'white', fontWeight: 'bolder', fontSize: '45px', position: 'absolute', marginTop: '-75px', letterSpacing: '4px'}}>LETS EXPLORE TOGETHER</p>
 					<button className="btn btn-lg view-btn" style={{zIndex: 10, margin: 'auto', position: 'absolute', color: 'white', paddingLeft: '50px', paddingRight: '50px', width: '211.45px', border: '1px solid white'}}>VIEW MORE</button>
 				</div>
-					<div className="col" style={{display: 'inline-flex'}}>
-						<div className="col-xs-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
-						<img src={require('../../../site-specs/sliced-images/package-thumb-01.png')} style={{height: 350, width: 480}}></img>
+					<div className="row">
+						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-01.png')})`}} className={`col-sm-4 ${style.featuredTile} ${style.bgDiv}`}>
 						</div>
-						<div className="col-xs-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
+						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-02.png')})`}} className={`col-sm-4 ${style.featuredTile}  ${style.bgDiv}`}>
+						</div>
+						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-03.png')})`}} className={`col-sm-4 ${style.featuredTile}  ${style.bgDiv}`}>
+						</div>
+						{/* <div className="col-xs-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
 						<img src={require('../../../site-specs/sliced-images/package-thumb-02.png')} style={{height: 350, width: 480}}></img>
 						</div>
 						<div className="col-xs-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
 						<img src={require('../../../site-specs/sliced-images/package-thumb-03.png')} style={{height: 350, width: 480}}></img>
-						</div>
+						</div> */}
 					</div>
 		</div>
 		)
