c484748b61c5e3e2232a4be4bb88e9f263b02448
Yasser Zubair <yasserzubair68@gmail.com>
2018-12-12 02:46:05 +0500

    div on tile

--- client/app/containers/Home/index.js
+++ client/app/containers/Home/index.js
@@ -18,12 +18,33 @@ class Home extends React.Component {
 					<p style={{color: 'white', fontWeight: 'bolder', fontSize: '45px', position: 'absolute', marginTop: '-75px', letterSpacing: '4px'}}>LETS EXPLORE TOGETHER</p>
 					<button className="btn btn-lg view-btn" style={{zIndex: 10, margin: 'auto', position: 'absolute', color: 'white', paddingLeft: '50px', paddingRight: '50px', width: '211.45px', border: '1px solid white'}}>VIEW MORE</button>
 				</div>
-					<div className="row">
+					<div className="col-sm-12 no-padding">
 						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-01.png')})`}} className={`col-sm-4 ${style.featuredTile} ${style.bgDiv}`}>
+							<div className={`${style.featuredTilePriceDiv}`}>
+								<p className={'no-padding no-margin'}>
+								<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>RS</p>
+								5000+ 
+									<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>/Night</p>
+								</p>
+							</div>
 						</div>
 						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-02.png')})`}} className={`col-sm-4 ${style.featuredTile}  ${style.bgDiv}`}>
+							<div className={`${style.featuredTilePriceDiv}`}>
+								<p className={'no-padding no-margin'}>
+								<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>RS</p>
+								5000+ 
+									<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>/Night</p>
+								</p>
+							</div>
 						</div>
 						<div style={{background: `url(${require('../../../site-specs/sliced-images/package-thumb-03.png')})`}} className={`col-sm-4 ${style.featuredTile}  ${style.bgDiv}`}>
+							<div className={`${style.featuredTilePriceDiv}`}>
+								<p className={'no-padding no-margin'}>
+								<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>RS</p>
+								5000+ 
+									<p style={{fontSize:'12px', color:'white', display: 'inline-block'}} className={'no-padding no-margin'}>/Night</p>
+								</p>
+							</div>
 						</div>
 						{/* <div className="col-xs-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
 						<img src={require('../../../site-specs/sliced-images/package-thumb-02.png')} style={{height: 350, width: 480}}></img>
