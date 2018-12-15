c8725b2a64f638c820dbeac7b406c4d51817175f
arslansajid <arslansajid951@gmail.com>
2018-12-15 20:54:24 +0500

    fixed footer styles

--- client/app/components/NewFooter/index.js
+++ client/app/components/NewFooter/index.js
@@ -5,20 +5,20 @@ import style from './style.css';
 class Footer extends Component {
   render() {
     return(
-        <div className={`container ${style.footer}`}>
+        <div className={`container text-center ${style.footer}`}>
           <div className="col-sm-4">
           <img style={{paddingTop: '7px'}} src={require('../../../site-specs/sliced-images/logo-footer.png')}></img>
           </div>
         <div className="col-sm-8">
-          <div className="col-sm-2"></div>
-          <div className="col-sm-10" style={{marginTop: '25px'}}> 
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Home </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Rooms </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Pages </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Blog </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Portfolio </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Shop </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Elements </a>
+          {/* <div className="col-sm-2"></div> */}
+          <div className="col-sm-12" style={{marginTop: '25px'}}> 
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> HOME </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> ROOMS </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> PAGES </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> BLOG </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> PORTFOLIO </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> SHOP </a>
+              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> ELEMENTS </a>
           </div>
         </div>
   </div>
