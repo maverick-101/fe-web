94cecbeef20a3b2544b91f594de9dae8dd5b6ac1
Yasser Zubair <yasserzubair68@gmail.com>
2019-01-07 03:19:31 +0500

    completed hotel page

--- client/app/containers/HotelPage/index.js
+++ client/app/containers/HotelPage/index.js
@@ -31,7 +31,19 @@ class HotelPage extends React.Component {
         {name:'TV', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-02.png')},
         {name:'Bathroom Accessories', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-03.png')},
         {name:'Bedroom Comforts', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-04.png')},
-    ]
+    ],
+    reviews: [
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+      {user: 'Yasser Zubair', image: require('../../../site-specs/sliced-images/recommended-thumb-01.png'), comments: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'},
+    ],
 		}
 	}
 
@@ -96,8 +108,53 @@ class HotelPage extends React.Component {
           <div className='row space-4'>
             <div className='col-sm-12'>
               <h1>Location</h1>
-              <div className='col-sm-12'>
-                <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+              <div className='row'>
+                <div className='col-sm-12'>
+                  <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row space-4'>
+            <div className='col-sm-12'>
+              <h1>Reviews</h1>
+              <div className='row'>
+                <div className='col-sm-12'>
+                  {
+                    this.data.reviews.map((review, index) => {
+                      return (
+                        <div className='col-sm-6 space-2 no-padding'>
+                          <div className='row space-1'>
+                            <div className='vcenter' style={{display: 'inline-block'}}>
+                              <div className={`bgDiv ${style.reviewImage}`} style={{background:`url(${review.image})` }}></div>
+                            </div>
+                            <div style={{display: 'inline-block', paddingTop: '15px'}} className='vcenter'>
+                              <h4 className='no-margin'>{review.user}</h4>
+                              <p>5 days ago</p>
+                            </div>
+                          </div>
+                          <div className='row'>
+                            <div className='col-sm-12'>{review.comments}</div>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                  {/* <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe> */}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row space-8'>
+          <div className='col-sm-12 space-4'>
+            <h1>Recommended Destinations For You</h1>
+            <div className='row'>
+            {this.data.hotelFeatures.map((image) => {
+              return <div className='col-sm-2 space-4'>
+                <div className={`bgDiv ${style.featureImage}`} style={{background:`url(${image.url})` }}></div>
+                <h4 style={{margin: '10px, 0'}}>{image.name}</h4>
+              </div>
+            })}
               </div>
             </div>
           </div>
