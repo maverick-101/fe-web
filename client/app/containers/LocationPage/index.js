b095f3e3dd5946f1783aedb925c268fa8a63645c
Yasser Zubair <yasserzubair68@gmail.com>
2019-02-25 00:49:48 +0500

    location page changed as per new api standards

--- client/app/containers/LocationPage/index.js
+++ client/app/containers/LocationPage/index.js
@@ -6,6 +6,9 @@ import HotelPackageTile from 'components/HotelPackageTile';
 import FeaturedHotelTile from 'components/FeaturedHotelTile';
 import RecommendationTile from 'components/RecommendationTile';
 import VisitedExperiences from 'components/VisitedExperiences';
+import Fader from 'components/Fader';
+import axios from 'axios';
+import config from 'config';
 
 import { DateRangePicker, isInclusivelyAfterDay, isInclusivelyBeforerDay } from 'react-dates';
 
@@ -19,7 +22,10 @@ class LocationPage extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
-      focusedInput: {},
+			focusedInput: {},
+			locations: [],
+			location: {},
+			travelerPackages: [],
     }
 		this.data = {
 			travelerPackages: [
@@ -62,16 +68,41 @@ class LocationPage extends React.Component {
 		}
 	}
 
+	componentDidMount() {
+
+		axios.get(`${config.apiPath}/fetchFeaturedPackages/featuredPackage-fetchFeaturedPackages`)
+		.then((response) => {
+			var travelerPackages = response.data;
+			console.log('traveler package', travelerPackages)
+			this.setState({
+				travelerPackages,
+			})
+		})
+
+		axios.get(`${config.apiPath}/fetch/locations-fetch`)
+		.then((response) => {
+			var locations = response.data;
+			console.log('locations', locations)
+			this.setState({
+				locations,
+			})
+		})
+		axios.get(`${config.apiPath}/fetchById/location-fetchById/${this.props.params.locationId}`)
+		.then((response) => {
+			var location = response.data[0];
+			console.log('locations', location[0])
+			this.setState({
+				location,
+			})
+		})
+		
+	}
+
 	render() {
+		var { location } = this.state;
+		console.log('location form satate', location)
     return (
 		<div>
-			{/* <div className="home-screen" style={{paddingLeft: '0', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-				<div className={style.homeCoverStyle} style={{background: `url(${require('../../../site-specs/sliced-images/background-bg.png')})`}}>
-        </div>
-        
-					<p style={{color: 'white', fontWeight: 'bolder', fontSize: '45px', position: 'absolute', marginTop: '-75px', letterSpacing: '4px'}}>LETS EXPLORE TOGETHER</p>
-					<button className="btn btn-lg view-btn" style={{zIndex: 10, margin: 'auto', position: 'absolute', color: 'white', paddingLeft: '50px', paddingRight: '50px', width: '211.45px', border: '1px solid white'}}>VIEW MORE</button>
-        </div> */}
         <div className={`col-sm-2 ${style.smallColumn}`}>
           <div className='space-4'></div>
           <div className='space-4'>
@@ -86,11 +117,9 @@ class LocationPage extends React.Component {
               startDatePlaceholderText = 'Check in'
               endDatePlaceholderText = 'Check Out'
               showDefaultInputIcon= {true}
-              // PropTypes.func.isRequired,
               focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
               onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
               numberOfMonths={2}
-              // isOutsideRange={day => isInclusivelyBeforerDay(day, moment().add(1, 'days'))}
             />
           </div>
           <div className='space-4'>
@@ -107,39 +136,42 @@ class LocationPage extends React.Component {
           </div>
         </div>
         <div className={`col-sm-10  ${style.largeColumn}`}>
-
 					<div className="container space-4">
-						<h1>Top Rated Experiences in Gilgit Baltistan</h1>
+						<h1>Top Rated Experiences in {location.name}</h1>
 						<p className='space-4'>Book activities led by local hosts on your next trip</p>
-						{this.data.visitedExperiences.map((data, index) => {
-							return <VisitedExperiences data={data} />
-						})}
+						<Fader width={250} maxWidth={1280} unSlickTill={1024} items=
+								{this.data.visitedExperiences.map((data, index) => {
+									return <VisitedExperiences data={data} />
+								})}></Fader>
 					</div>
 					<div className="container space-4">
 						<h4 style={{color: 'orange'}}>Show all experiences</h4>
 					</div>
           <div className="container space-4">
-						<h1>Top Visited Experiences Exp Gilgit Baltistan</h1>
+						<h1>Top Visited Experiences Exp {location.name}</h1>
 						<p className='space-4'>Book activities led by local hosts on your next trip</p>
-						{this.data.visitedExperiences.map((data, index) => {
-							return <VisitedExperiences data={data} />
-						})}
+						<Fader width={250} maxWidth={1280} unSlickTill={1024} items=
+								{this.data.visitedExperiences.map((data, index) => {
+									return <VisitedExperiences data={data} />
+						})}></Fader>
 					</div>
 					<div className="container space-4">
 						<h4 style={{color: 'orange'}}>Show all experiences</h4>
 					</div>
           <div className="container space-4">
 						<h1>Recommended for you</h1>
-						{this.data.recommendations.map((data, index) => {
+						<Fader width={250} maxWidth={1280} unSlickTill={1024} items=
+						{this.state.locations.map((data, index) => {
 							return <RecommendationTile data={data} />
-						})}
+						})}></Fader>
 					</div>
           <div className="container space-4">
-						<h1>Popular Guides in Gilgit Baltistan</h1>
+						<h1>Popular Guides in {location.name}</h1>
 						<p className='space-4'>Discover places with one of these popular guides</p>
-						{this.data.travelerPackages.map((data, index) => {
+						<Fader width={340} maxWidth={1280} unSlickTill={1024} items=
+						{this.state.travelerPackages.map((data, index) => {
 							return <TravelerPackageTile data={data} />
-						})}
+						})}></Fader>
 					</div>
 
         </div>
