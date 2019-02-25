353517f8fd65f406607f36a54b1d0955ddcd0721
Yasser Zubair <yasserzubair68@gmail.com>
2019-02-25 23:25:53 +0500

    a tile for displaying rooms on contact card modal

---
+++ client/app/components/HotelContactCard/RoomTile.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Card, CardText, CardBody, CardTitle, CardImg, Button} from 'reactstrap';
+import TravelerPackageTile from 'components/TravelerPackageTile';
+import HotelPackageTile from 'components/HotelPackageTile';
+import placeholder from 'no-image.jpg';
+
+import style from './style.css'
+
+
+class Tile extends React.Component {
+  render() {
+    var { room, selectedId } = this.props;
+    return (  
+      // <a href={`/hotel/${this.props.data.ID}`}>
+      <div className='row text-left space-4'>
+      <div className={`col-sm-12`}>
+        <div className={`col-sm-12 no-padding ${style.roomTile} ${selectedId == room.ID ? style.roomTileActive : ''}`}>
+        <div className='col-sm-6 no-padding'>
+          <div className={`bgDiv ${style.roomTileImage}`} style={{height: '165px', background: `url(${this.props.image || placeholder})`}}></div>
+        </div>
+        <div className='col-sm-6'>
+          <h3 className='ellipses'>{room.title}</h3>
+          <p className='ellipses'><b>Bed Type : </b> {room.bed_type} </p>
+          <p  className='ellipses'><b>Beds : </b> {room.beds} </p>
+          <h5 className={`${style.roomPrice}`} >Rs. 3500/Night</h5>
+        </div>
+        </div>
+        </div>
+      </div>
+      //  </a>
+    )
+  }
+}
+
+  export default Tile
