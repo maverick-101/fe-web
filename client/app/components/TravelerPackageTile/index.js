8894956762dbb54c163dae440dfc29a7f1100d35
Yasser Zubair <yasserzubair68@gmail.com>
2019-02-26 13:02:57 +0500

    traveler package tile fixed

--- client/app/components/TravelerPackageTile/index.js
+++ client/app/components/TravelerPackageTile/index.js
@@ -16,13 +16,24 @@ class Tile extends React.Component {
           <div style={{position: 'relative'}}>
             <p className={`ellipses ${style.tileText}`}>{this.props.data.name}</p>
           </div>
-          <div className={style.guidePackagesText}>
+          <div className={`clearfix ${style.guidePackagesText}`}>
+              <h4 className='ellipses text-left' style={{marginBottom: '4px'}}>{this.props.data.package_title}</h4>
             <div className='col-sm-7 no-padding-left text-left col-xs-7 col-md-7'>
-              <h4 className='ellipses' style={{marginBottom: '6px'}}>{this.props.data.package_title}</h4>
-              <p style={{marginBottom: '6px', fontWeight: '200', fontSize: '14px'}}>Starting Price <span style={{color: '#e3530d'}}>Rs.{3000}</span></p>
+              <p style={{marginBottom: '0px', fontWeight: '200', fontSize: '12px'}}>Starting Price</p>
+              <h5 style={{color: '#e3530d', marginTop:'0px'}}>Rs.{3000}</h5>
             </div>
-              <button style={{backgroundColor: '#00b3b3', color: 'white', fontSize:'14px', border: 'none', marginTop: '5px'}} className="btn col-xs-5 col-sm-5 col-md-5 btn-lg btn-primary">Book Now</button>
+              <button style={{backgroundColor: '#00b3b3', color: 'white', fontSize:'14px', border: 'none'}} className="btn col-xs-5 col-sm-5 col-md-5 btn-lg btn-primary">Book Now</button>
           </div>
+
+          {/* <div className={`clearfix ${style.hotelPackagesText}`}>
+            <h4 className='ellipses' style={{marginBottom: '4px'}}>{this.props.data.name}</h4>
+            <div style={{marginTop: '3px'}} className='col-sm-7 no-padding col-xs-7 col-md-7'>
+              <p className='ellipses' style={{marginBottom: '0px', fontWeight: '200', fontSize: '12px'}}>Starting Price</p>
+              <h5 style={{color: '#e3530d', marginTop:'0px'}}>Rs.{this.props.data.minimum_price || 'N/A'}</h5>
+            </div>
+              <button style={{backgroundColor: '#00b3b3', color: 'white', border: 'none', fontSize: '14px'}} className="col-sm-5 col-xs-5 ellipses btn btn-lg btn-primary">Book Now</button>
+          </div> */}
+
         </div>
       </div>
       </a>
