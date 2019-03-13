e356fdaeac0868b2888acd07d19e2c4155cf9094
Yasser Zubair <yasserzubair68@gmail.com>
2019-03-13 11:30:20 +0500

    footer enties fixed

--- client/app/components/NewFooter/index.js
+++ client/app/components/NewFooter/index.js
@@ -6,18 +6,18 @@ class Footer extends Component {
   render() {
     return(
         <div className={`container text-center ${style.footer}`}>
-          <div className="text-left col-sm-4">
-          <img style={{paddingTop: '7px'}} src={require('../../../site-specs/sliced-images/logo-footer.png')}></img>
+          <div className="text-center col-sm-4">
+          <img style={{paddingTop: '20px'}} src={require('../../../site-specs/sliced-images/logo-footer.png')}></img>
           </div>
-        <div className="text-right col-sm-8">
+        <div className="text-right hidden-xs col-sm-8">
           {/* <div className="col-sm-2"></div> */}
-          <div className="text-right col-sm-12" style={{marginTop: '25px'}}> 
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Home </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Hotels </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Packages </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Events </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Blog </a>
-              <a style={{fontSize: '18px', paddingRight: '35px'}} href="mailto:bookings@roomy.pk" target="new"> Contact Us </a>
+          <div className="text-right col-sm-12" style={{marginTop: '35px'}}> 
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Home </a>
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Hotels </a>
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Packages </a>
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Events </a>
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Blog </a>
+              <a style={{fontSize: '18px', margin: '15px'}} className={style.footerLinks} href="mailto:bookings@roomy.pk" target="new"> Contact Us </a>
           </div>
         </div>
   </div>
