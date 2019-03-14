a7d37522931f41429b76aa214878de8a1008f51c
Yasser Zubair <yasserzubair68@gmail.com>
2019-03-14 16:57:33 +0500

    added price

--- client/app/components/HotelPackageTile/index.js
+++ client/app/components/HotelPackageTile/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Card, CardText, CardBody, CardTitle, CardImg, Button} from 'reactstrap';
+import { convertPrice } from 'helpers'
 
 import style from './style.css'
 
@@ -17,7 +18,7 @@ class Tile extends React.Component {
             <h4 className='ellipses' style={{marginBottom: '4px'}}>{this.props.data.name}</h4>
             <div style={{marginTop: '3px'}} className='col-sm-7 no-padding col-xs-7 col-md-7'>
               <p className='ellipses' style={{marginBottom: '0px', fontWeight: '200', fontSize: '12px'}}>Starting Price</p>
-              <h5 style={{color: '#e3530d', marginTop:'0px'}}>Rs.{this.props.data.minimum_price || 'N/A'}</h5>
+              <h5 style={{color: '#e3530d', marginTop:'0px'}}>Rs.{convertPrice(this.props.data.minimum_price, 'PKR') || 'N/A'}</h5>
             </div>
               <button style={{backgroundColor: '#00b3b3', color: 'white', border: 'none', fontSize: '14px'}} className="col-sm-5 col-xs-5 ellipses btn btn-lg btn-primary">Book Now</button>
           </div>
