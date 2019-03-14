55d03f63f928ff14aa29b5fb2850f988529c1eea
Yasser Zubair <yasserzubair68@gmail.com>
2019-03-14 16:59:10 +0500

    removed redundant code

--- client/app/components/TravelerPackageTile/index.js
+++ client/app/components/TravelerPackageTile/index.js
@@ -25,16 +25,6 @@ class Tile extends React.Component {
             </div>
               <button style={{backgroundColor: '#00b3b3', color: 'white', fontSize:'14px', border: 'none'}} className="btn col-xs-5 col-sm-5 col-md-5 btn-lg btn-primary">Book Now</button>
           </div>
-
-          {/* <div className={`clearfix ${style.hotelPackagesText}`}>
-            <h4 className='ellipses' style={{marginBottom: '4px'}}>{this.props.data.name}</h4>
-            <div style={{marginTop: '3px'}} className='col-sm-7 no-padding col-xs-7 col-md-7'>
-              <p className='ellipses' style={{marginBottom: '0px', fontWeight: '200', fontSize: '12px'}}>Starting Price</p>
-              <h5 style={{color: '#e3530d', marginTop:'0px'}}>Rs.{this.props.data.minimum_price || 'N/A'}</h5>
-            </div>
-              <button style={{backgroundColor: '#00b3b3', color: 'white', border: 'none', fontSize: '14px'}} className="col-sm-5 col-xs-5 ellipses btn btn-lg btn-primary">Book Now</button>
-          </div> */}
-
         </div>
       </div>
       </a>
