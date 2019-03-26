8d056b03d4715d700bd2796343c32acd752ad529
Yasser Zubair <yasserzubair68@gmail.com>
2019-03-26 10:30:46 +0500

    experiences route

--- client/app/app.js
+++ client/app/app.js
@@ -62,7 +62,7 @@ ReactDOM.hydrate(<Provider store={store}>
 				<Route path="/hotel/:hotelId" components={{full: HotelPage}}/>
 				<Route path="/location/:locationId" components={{full: LocationPage}}/>
 				<Route path="/package/:packageId" components={{full: PackagePage}}/>
-				<Route path="/experience/:experienceId" components={{full: PackagePage}}/>
+				<Route path="/experience/:experienceId" components={{full: ExperiencePage}}/>
 			</Route>
 		</Router>
 	</Provider>,
