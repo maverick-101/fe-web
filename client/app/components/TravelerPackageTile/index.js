a3f7cc5c18ec60fee8dc23c309b06318bc849e42
Yasser Zubair <yasserzubair68@gmail.com>
2019-04-03 12:52:28 +0500

    helper for resized images

--- client/app/components/TravelerPackageTile/index.js
+++ client/app/components/TravelerPackageTile/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import placeholder from 'no-image.jpg'
-import { convertPrice } from 'helpers'
+import { convertPrice, imgUpload } from 'helpers'
 import { Card, CardText, CardBody, CardTitle, CardImg, Button} from 'reactstrap';
 
 import style from './style.css'
@@ -12,7 +12,7 @@ class Tile extends React.Component {
       <a href={`/package/${this.props.data.ID}`}>
       <div style={{width:'100%'}} className='space-4 inline-block'>
         <div className={`${style.guidePackagesTile}`}>
-          <div className={`bgDiv ${style.guidePackagesImg}`} style={{background:`url(${this.props.data.gallery.length ? this.props.data.gallery[0].url : placeholder })` }}>
+          <div className={`bgDiv ${style.guidePackagesImg}`} style={{background:`url(${this.props.data.gallery.length ? imgUpload(this.props.data.gallery[0].url, 'h_400') : placeholder })` }}>
           </div>
           <div style={{position: 'relative'}}>
             <p className={`ellipses ${style.tileText}`}>{this.props.data.name}</p>
