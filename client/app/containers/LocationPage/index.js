f0b601966da75a317ef025d5ce47966c920b819c
Yasser Zubair <yasserzubair68@gmail.com>
2019-04-09 20:19:34 +0500

    removed static data

--- client/app/containers/LocationPage/index.js
+++ client/app/containers/LocationPage/index.js
@@ -10,6 +10,8 @@ import Fader from 'components/Fader';
 import axios from 'axios';
 import config from 'config';
 import _ from 'lodash';
+import { convertPrice, imgUpload } from 'helpers'
+
 
 import { DateRangePicker, isInclusivelyAfterDay, isInclusivelyBeforerDay } from 'react-dates';
 
@@ -30,65 +32,10 @@ class LocationPage extends React.Component {
 			hotelPackages: [],
 			experiences: [],
     }
-		this.data = {
-			travelerPackages: [
-				{name:'3 Days Trip to Hunza Valley', location: 'Hunza Valley', price:3000, url: require('../../../site-specs/sliced-images/traveller-package-01.png')},
-				{name:'5 Days Trip to Shangrila', location: 'Shangrilla Resorts', price:3000, url: require('../../../site-specs/sliced-images/traveller-package-02.png')},
-				{name:'3 Days Trip to Naran Kaghan', location: 'Naran Kaghan Valley', price:3000, url: require('../../../site-specs/sliced-images/traveller-package-03.png')},
-		],
-		hotelPackages: [
-			{name:'3 Days Trip to Hunza Valley', location: 'Hunza Valley', price:3000, url: require('../../../site-specs/sliced-images/hotel-resort-01.png')},
-			{name:'5 Days Trip to Shangrila', location: 'Shangrilla Resorts', price:3000, url: require('../../../site-specs/sliced-images/hotel-resort-02.png')},
-			{name:'3 Days Trip to Naran Kaghan', location: 'Naran Kaghan Valley', price:3000, url: require('../../../site-specs/sliced-images/hotel-resort-03.png')},
-		],
-		featuredHotels: [
-			{name:'3 Days Trip to Hunza Valley', location: 'Hunza Valley', min_price:3000, url: require('../../../site-specs/sliced-images/package-thumb-01.png')},
-			{name:'5 Days Trip to Shangrila', location: 'Shangrilla Resorts', min_price:5000, url: require('../../../site-specs/sliced-images/package-thumb-02.png')},
-			{name:'3 Days Trip to Naran Kaghan', location: 'Naran Kaghan Valley', min_price:7000, url: require('../../../site-specs/sliced-images/package-thumb-03.png')},
-		],
-		featuredHotels: [
-			{name:'3 Days Trip to Hunza Valley', location: 'Hunza Valley', min_price:3000, url: require('../../../site-specs/sliced-images/package-thumb-01.png')},
-			{name:'5 Days Trip to Shangrila', location: 'Shangrilla Resorts', min_price:5000, url: require('../../../site-specs/sliced-images/package-thumb-02.png')},
-			{name:'3 Days Trip to Naran Kaghan', location: 'Naran Kaghan Valley', min_price:7000, url: require('../../../site-specs/sliced-images/package-thumb-03.png')},
-		],
-		recommendations: [
-			{name:'Faisal Mosque', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-01.png')},
-			{name:'Baltit Fort', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-02.png')},
-			{name:'Sindh Fort', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-03.png')},
-			{name:'Shah Rukn Tomb', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-04.png')},
-			{name:'Lahore Fort', reviews: '684 reviews', url: require('../../../site-specs/sliced-images/recommended-thumb-05.png')},
-		],
-		visitedExperiences: [
-			{name:'Chillas Valley', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-01.png')},
-			{name:'Islamabad Expressway', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-02.png')},
-			{name:'Skardu Hotels', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-03.png')},
-			{name:'Banjosa Lake', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-04.png')},
-			{name:'Lahore Fort', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-05.png')},
-			{name:'Islamabad Expressway', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-06.png')},
-			{name:'Skardu Hotels', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-07.png')},
-			{name:'Banjosa Lake', reviews: '684 reviews', rating: '3.5', url: require('../../../site-specs/sliced-images/top-visited-08.png')},
-		],
-		}
 	}
 
 	componentDidMount() {
 		this.props.done();
-		// axios.get(`${config.apiPath}/fetchFeaturedPackages/featuredPackage-fetchFeaturedPackages`)
-		// .then((response) => {
-		// 	var travelerPackages = response.data;
-		// 	this.setState({
-		// 		travelerPackages,
-		// 	})
-		// })
-
-		// axios.get(`${config.apiPath}/fetch/locations-fetch`)
-		// .then((response) => {
-		// 	var locations = response.data;
-		// 	this.setState({
-		// 		locations,
-		// 	})
-		// })
-
 		axios.get(`${config.apiPath}/hotel/fetchByLocation/${this.props.params.locationId}`)
 		.then((response) => {
 			var hotelPackages = response.data.map((item) => {
@@ -142,47 +89,7 @@ class LocationPage extends React.Component {
 		console.log('location form satate', location)
     return (
 		<div>
-        {/* <div className={`col-sm-2 ${style.smallColumn}`}>
-          <div className='space-4'></div>
-          <div className='space-4'>
-            <h5 className='space-2'>Dates</h5>
-            <DateRangePicker
-              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-              withPortal= {true}
-              onDatesChange={({ startDate, endDate }) => {
-                this.changeDates(startDate, endDate);
-              }}
-              startDatePlaceholderText = 'Check in'
-              endDatePlaceholderText = 'Check Out'
-              showDefaultInputIcon= {true}
-              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-              numberOfMonths={2}
-            />
-          </div>
-          <div className='space-4'>
-            <h5 className='space-2'>Guests</h5>
-            <div style={{position: 'relative'}}>
-              <select name="month" className={`form-control ${style.guestSelect}`}>
-                <option value="">Guests</option>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
-                  (number, index) => <option key={index} value={number}>{number}</option>
-                )}
-              </select>
-              <i style={{color: '#00b3b3', position: 'absolute', top: 10, right: 10}} className="fa fa-chevron-down"></i>
-            </div>
-          </div>
-        </div> */}
         <div className={`${style.largeColumn}`}>
-					{/* <div className="container space-4">
-						<h1>Top Rated Experiences in {location.name}</h1>
-						<p className='space-4'>Book activities led by local hosts on your next trip</p>
-						<Fader width={250} maxWidth={1280} unSlickTill={1024} items=
-								{this.data.visitedExperiences.map((data, index) => {
-									return <VisitedExperiences data={data} />
-								})}></Fader>
-					</div> */}
 					{ 
 					this.state.hotelPackages.length ?
 					<div className="container space-4">
