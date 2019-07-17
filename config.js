67eb4486ae8faee26dd25405169f3c2c26898be2
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-17 16:28:42 +0500

    added path

--- config.js
+++ config.js
@@ -2,13 +2,14 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
   development: {
-    apiPath: 'https://api.saaditrips.com/api',
+    apiPath: '',
     domain: '',
     revivePath: '',
     lockScreen: false,
   },
   production: {
-    apiPath: 'https://saaditrips.com/api',
+    // apiPath: 'https://saaditrips.com/api',
+    apiPath: '',
     domain: 'saaditrips.com',
     revivePath: '',
     lockScreen: false,
